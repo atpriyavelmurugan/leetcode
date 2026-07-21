3b0e5bcf43c9192e5c23b89b51b667a5f14cb180
atpriyavelmurugan <priyavelmurugan24_bcs28@mepcoeng.ac.in>
2026-07-21 16:30:35 +0530

    Create firstmissing.py

---
+++ week2/firstmissing.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        
+        n = len(nums)
+        
+        for i in range(n):
+           
+            while 0 < nums[i] <= n and nums[nums[i] - 1] != nums[i]:
+                
+                target_idx = nums[i] - 1
+                nums[i], nums[target_idx] = nums[target_idx], nums[i]
+                
+        
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+                
+        
+        return n + 1
