519469c06f0b07126d4d7e9d53fb67c3439f0ab8
atpriyavelmurugan <priyavelmurugan24_bcs28@mepcoeng.ac.in>
2026-08-11 15:32:48 +0530

    Create postorder.py

---
+++ week5/postorder.py
@@ -0,0 +1,17 @@
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+            
+        stack = [root]
+        result = []
+        
+        while stack:
+            node = stack.pop()
+            result.append(node.val)
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+   
+        return result[::-1]
